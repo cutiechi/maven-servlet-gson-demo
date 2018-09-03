8baa114d31d31faaf94f7dedad8ed720caeade4f
cutiechi <chi@cutie.ooo>
2018-09-03 15:02:43 +0800

    feat(service): add list all user method

--- src/main/java/com/cutiechi/demo/service/UserService.java
+++ src/main/java/com/cutiechi/demo/service/UserService.java
@@ -1,5 +1,11 @@
 package com.cutiechi.demo.service;
 
+import com.cutiechi.demo.model.dto.ServiceResult;
+import com.cutiechi.demo.model.entity.User;
+
+import java.util.List;
+
 public interface UserService {
 
+    ServiceResult<List<User>> listAll ();
 }
--- src/main/java/com/cutiechi/demo/service/impl/UserServiceImpl.java
+++ src/main/java/com/cutiechi/demo/service/impl/UserServiceImpl.java
@@ -1,7 +1,23 @@
 package com.cutiechi.demo.service.impl;
 
+import com.cutiechi.demo.dao.UserDao;
+import com.cutiechi.demo.dao.impl.UserDaoImpl;
+import com.cutiechi.demo.model.dto.ServiceResult;
+import com.cutiechi.demo.model.entity.User;
 import com.cutiechi.demo.service.UserService;
 
-public class UserServiceImpl implements UserService {
+import java.util.List;
 
+public class UserServiceImpl implements UserService {
+    
+    private UserDao userDao;
+    
+    public UserServiceImpl () {
+        this.userDao = new UserDaoImpl();
+    }
+    
+    @Override
+    public ServiceResult<List<User>> listAll () {
+        return new ServiceResult<>(true, "获取全部用户列表成功！", userDao.listAll());
+    }
 }
