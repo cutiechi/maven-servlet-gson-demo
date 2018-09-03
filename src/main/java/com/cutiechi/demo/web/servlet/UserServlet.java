ced414912f36bf92bcfc505ae54ec8c846400a8a
cutiechi <chi@cutie.ooo>
2018-09-03 15:03:40 +0800

    feat(servlet): add user servlet

---
+++ src/main/java/com/cutiechi/demo/web/servlet/UserServlet.java
@@ -0,0 +1,9 @@
+package com.cutiechi.demo.web.servlet;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(name = "UserServlet", urlPatterns = "/users")
+public class UserServlet extends HttpServlet {
+
+}
